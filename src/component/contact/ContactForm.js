27ff61c050fc7c6897e4cabd992eac6d19cbb452
Trond Magnus Sevre <trond@fintlabs.no>
2024-05-03 10:20:44 +0200

    Make textfields outlined

--- src/component/contact/ContactForm.js
+++ src/component/contact/ContactForm.js
@@ -6,52 +6,67 @@ const ContactForm = ({nin, contact, updateContactState, createContact, isFormVal
     const [showNin, setShowNin] = useState(false);
     return (
         <React.Fragment>
-            <TextField
-                name="nin"
-                label="Fødselsnummer (hold musen over for å vise)"
-                fullWidth
-                disabled
-                inputProps={{
-                    style: {
-                        visibility: showNin ? 'visible' : 'hidden'
-                    }
-                }}
-                value={nin ? nin : undefined}
-                onMouseEnter={() => setShowNin(true)}
-                onMouseLeave={() => setShowNin(false)}
-            />
-            <TextField
-                name="firstName"
-                label="Fornavn"
-                required
-                fullWidth
-                value={contact ? contact.firstName : undefined}
-                onChange={updateContactState}
-            />
-            <TextField
-                name="lastName"
-                label="Etternavn"
-                required
-                fullWidth
-                value={contact ? contact.lastName : undefined}
-                onChange={updateContactState}
-            />
-            <TextField
-                name="mail"
-                label="E-post"
-                required
-                fullWidth
-                value={contact ? contact.mail : undefined}
-                onChange={updateContactState}
-            />
-            <TextField
-                name="mobile"
-                label="Mobil"
-                required
-                fullWidth
-                value={contact ? contact.mobile : undefined}
-                onChange={updateContactState}
-            />
+            <Box mb={1}>
+                <TextField
+                    name="nin"
+                    label="Fødselsnummer (hold musen over for å vise)"
+                    variant="outlined"
+                    fullWidth
+                    disabled
+                    inputProps={{
+                        style: {
+                            visibility: showNin ? 'visible' : 'hidden'
+                        }
+                    }}
+                    value={nin ? nin : undefined}
+                    onMouseEnter={() => setShowNin(true)}
+                    onMouseLeave={() => setShowNin(false)}
+                />
+            </Box>
+            <Box mb={1}>
+                <TextField
+                    name="firstName"
+                    label="Fornavn"
+                    variant="outlined"
+                    required
+                    fullWidth
+                    value={contact ? contact.firstName : undefined}
+                    onChange={updateContactState}
+                />
+            </Box>
+            <Box mb={1}>
+                <TextField
+                    name="lastName"
+                    label="Etternavn"
+                    variant="outlined"
+                    required
+                    fullWidth
+                    value={contact ? contact.lastName : undefined}
+                    onChange={updateContactState}
+                />
+            </Box>
+            <Box mb={1}>
+                <TextField
+                    name="mail"
+                    label="E-post"
+                    variant="outlined"
+                    required
+                    fullWidth
+                    value={contact ? contact.mail : undefined}
+                    onChange={updateContactState}
+                />
+            </Box>
+            <Box mb={1}>
+                <TextField
+                    name="mobile"
+                    label="Mobil"
+                    variant="outlined"
+                    required
+                    fullWidth
+                    value={contact ? contact.mobile : undefined}
+                    onChange={updateContactState}
+                />
+            </Box>
             <Box mt={2} display="flex" justifyContent="flex-end">
                 <Button variant="contained" disabled={!isFormValid()}
                         onClick={() => createContact()} color="primary">
